a8319d00bf87b1fb7acec5207b8594a62af9c880
kwonyounghye <kyh90721@gmail.com>
2023-09-08 17:18:25 +0900

    0908

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -1,5 +1,61 @@
 // CGV PJ 메인 페이지 JS - main.js
 
-// 요소선택함수 ////////
-const qs = (x) => document.querySelector(x);
-const qsa = (x) => document.querySelectorAll(x);
+// DOM 함수 객체 //////////////
+const domFn = {
+    // 요소선택함수 ////////
+    // qs/qsa을 길게 안쓰려고 사용
+    qs: (x) => document.querySelector(x),
+    qsEl: (el, x) => el.querySelector(x),
+    qsa: (x) => document.querySelectorAll(x),
+    qsaEl: (el, x) => el.querySelectorAll(x),
+
+    // 이벤트 셋팅 함수
+    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
+}; ////////////// domFn 객체 ///////////////////////////
+
+// 구현1.
+// [ 포스터 메뉴를 클릭하여 메인 유튜브 이미지를 변경한다! ]
+// 1. 대상
+// 1-1. 이벤트 대상 : .post-menu a
+const pmenu = domFn.qsa(".post-menu a");
+// 1-2. 변경대상 : .screen
+const screen = domFn.qs(".screen");
+
+console.log("대상: ", pmenu, screen);
+
+// 2. 데이터 생성하기
+// 각 영화별 아이디 객체 만들기
+
+const mvCode = {
+    "닥터스트레인지2": "mI9oyFMUlfg",
+    "쥬라기월드:도미니언": "DSEfRVqjbFA",
+    "브로커": "DpVAb7Bi5UQ",
+    "범죄도시2": "aw9j_23nORs",
+    // "몬스터싱어": "ELuQ4sMp4yw",
+    "하울의 움직이는 성": "iwROgK94zcM",
+    "스파이더맨:노웨이홈": "W7edvITC9g4",
+};
+
+// 3. 이벤트 함수 만들기
+// 이벤트 대상에 클릭 설정하여 함수 연결하기
+pmenu.forEach((ele) => {
+    domFn.addEvt(ele, "click", (e) => {
+        // 나자신!
+        let me = e.currentTarget;
+        // 1, 클릭된 a요소 하위 영화제목 정보 읽어오기
+        let mtit = domFn.qsEl(ele, "li").innerText;
+        // 호출 확인!
+        console.log("나야나야", mtit, mvCode[mtit]);
+        // 2. 스크린 박스에 아이프레임 src 재구성하여 넣기
+        screen.innerHTML = `
+        <iframe src="https://www.youtube.com/embed/${mvCode[mtit]}?autoplay=1&mute=1&loop=1&playlist=vteD1f-SV5I" allow="autoplay"></iframe>    
+        `;
+
+        // 3. 추가 구현 : 클릭된 a요소의 부모인 li에 클래스 on주기
+        // - on을 주면 미리 셋팅된대로 li가 일어나 있다!
+        // 부모로 올라가기  ele.parentElement
+
+        // 3-1. 클래스 on 지우기 초기화 : 대상 mlist변수
+        mlist.forEach
+    });
+}); //////////// forEach /////////////////
--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -1,21 +1,52 @@
-window.addEventListener('DOMContentLoaded', loadFn);
+window.addEventListener("DOMContentLoaded", loadFn);
 
 // 로딩 구역 함수 //////////////////////////////////////////////
 function loadFn() {
     // 로딩 확인
-    console.log('로딩 확인');
+    console.log("로딩 확인");
 
     startSS();
 
     // 부드러운 스크롤 때문에 마우스휠 이벤트 막기
 
     // 대상: .desc-box
-    let desc_box = document.querySelectorAll('.desc-box');
+    let desc_box = document.querySelectorAll(".desc-box");
 
     // 모든 캐릭터 설명박스는 이벤트 버블링 막기
     // -> 여기서 마우스휠 됨!
-    desc_box.forEach(ele=>{ /* ele - 요소 자신 */
-        ele.onwheel=e=>e.stopPropagation()
-});
+    desc_box.forEach((ele) => {
+        /* ele - 요소 자신 */
+        ele.onwheel = (e) => e.stopPropagation();
+    });
+
+    /**********************************************
+    [ 현장포토 파트 데이터 구성하기 ]
+    - 배열 데이터를 이용하여 HTML 코드 구성
+**********************************************/
+
+    // 1. 대상 선정 : .live-box
+    const liveBox = domFn.qs(".live-box");
+    console.log("대상: ", liveBox);
+
+    // 2. 현장포토 데이터를 기반으로 HTML코드 만들기
+    let hcode = "<ul>";
+
+    // 반복코드 만들기 //////////
+    // 현장포토 데이터 - data_drama.js에서 가져옴
+    liveData.forEach(val=>{
+        // html 변수에 계속 넣기
+        hcode +=
+        `   
+                <li>
+                    <figure>
+                        <img src="images/live_photo/${val.imgName}" alt="${val.title}">
+                        <figcaption>${val.title}</figcaption>
+                    </figure>
+                </li>
+        `;
+    })
 } ////////////// loadFn 함수 ///////////////////////
+hcode += '</ul>';
+
+console.log('hcode: ', hcode);
 ////////////////////////////////////////////////////
