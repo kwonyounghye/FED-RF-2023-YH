1b8509c4a294c5e132d23679ea19f0520b777ba9
kwonyounghye <kyh90721@gmail.com>
2023-09-12 17:17:05 +0900

    0912

--- 001.HTML학습/09.ShoppingMall_PJ/js/main3.js
+++ 001.HTML학습/09.ShoppingMall_PJ/js/main3.js
@@ -37,5 +37,66 @@ addEvt(window,"DOMContentLoaded", loadFn);
 ******************************************/
 function loadFn() {
     console.log("로딩완료!");
+    // 슬라이드 순번 전역변수
+    let snum=0;
 
+    // 1. 대상 선정
+    // 1-1. 이벤트 대상 : .abtn
+    const abtn = qsa('.abtn');
+    // 1-2. 변경 대상: #slide
+    const slide = qsa('#slide li');
+    // 슬라이드 개수
+    const CNT_SLIDE = slide.length;
+    // 1-3. 블릿박스 대상: 
+    const indic = qsa('.indic li');
+
+    // 대상 확인
+    console.log('대상', abtn, slide, indic);
+
+    // 2. 이벤트 설정하기
+    abtn.forEach(ele=>{
+        addEvt(ele,'click',goSlide);
+
+        // 3. 함수만들기
+        function goSlide() {
+            console.log('나야나~',this);
+
+            // 0. 이전 순번 li 클래스 "on" 지우기
+            slide[snum].classList.remove('on');
+
+
+            // 1. 버튼 구분하기
+            let isR = this.classList.contains('ab2')
+            console.log('오른쪽?',isR);
+            // 2. 버튼별 분기하기
+            if(isR) { // 오른쪽 버튼
+                // 슬라이드 번호 증가
+                snum++;
+                // 슬라이드 한계값 -> 끝이상은 처음으로!
+                if(snum==CNT_SLIDE) snum = 0;
+            } ///// if //////////
+            else { //////// 왼쪽 버튼
+                // 슬라이드 번호 감소
+                snum--;
+                // 슬라이드 한계값 -> 0미만은 마지막 번호!
+                if(snum<0) snum = CNT_SLIDE-1;
+
+            } //////////// else ////////////////
+
+            console.log('snum: ', snum);
+            // 3. 해당 li에 클래스 "on"넣기
+            slide[snum].classList.add('on');
+
+            // 0번에서 처리 안할 경우 4번에서 처리
+            // 4. 해당 li 빼고 전체 li 클래스 "on" 지우기
+            // 같은 li 요소인지 검증: 요소.isSameNode(다른요소)
+            slide.forEach(ele=>{
+                // console.log(ele.isSameNode(slide[snum]))
+                if(!ele.isSameNode(slide[snum]))
+                ele.classList.remove("on");
+
+            })
+            
+        } ///////////// goSlide 함수 //////////////
+    }); ////////////// forEach /////////////
 }/////////////// loadFn 함수 //////////////
