3ae2dd86c9077c6c5407d05ee959441a2b4eea96
kwonyounghye <kyh90721@gmail.com>
2023-09-27 18:15:48 +0900

    0927

--- 003.JS학습/01.JS실험실/js/cube2.js
+++ 003.JS학습/01.JS실험실/js/cube2.js
@@ -9,19 +9,23 @@
     - 사용이벤트: wheel
     - 단위 각도: 360도 / 9개 = 40도
     - CSS 이징 적용: ease-out
-************************************************/
-// DOM 함수 객체 //////////////
-const domFn = {
-    // 요소선택함수 ////////
-    qs: (x) => document.querySelector(x),
-    qsEl: (el, x) => el.querySelector(x),
-    qsa: (x) => document.querySelectorAll(x),
-    qsaEl: (el, x) => el.querySelectorAll(x),
-  
-    // 이벤트셋팅함수
-    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
-  }; /////// domFn 객체 /////////////
 
+    [ 캐릭터별 고유번호 만들기 ]
+    - 전역변수를 만들어 고유번호 저장
+    - 스크롤 방향에 따른 고유번호 증감
+    - 고유번호를 순환하여 항상 고정번호 만들기
+    -> 9개니까 0~8까지의 고유번호가 순환한다!
+
+************************************************/
+// DOM 메서드 모듈
+import domFn from './dom.js';
+
+// 데이터 제이슨 불러오기
+import mvData from './data_moving.json' assert{type:'json'};
+import dom from './dom.js';
+
+// console.log(mvData);
+  
 // 0. 변수 셋팅
 // 단위 각도
 const DEG = 40;
@@ -30,13 +34,23 @@ let stsWheel = 0;
 // 휠 단위 수(휠할 때 증감하는 수)
 let numWheel = 0;
 // 휠 제어 시간
-const TIME_WHEEL = 20; // 대문자는 함수
+const TIME_WHEEL = 120; // 대문자는 함수
 // 휠 단위수(휠할 때 증감하는 수)
 let nowWheel=0;
+// 캐릭터 고유번호
+let catNum = 0;
+// 캐릭터 번호한계수(9개니까 한계수 8)
+const LIMIT_CNT = 8;
+// 캐릭터 정보함수 호출 타임아웃용 변수
+let autoT;
+
 
 // 1. 대상 선정 : .cube
 const cube = domFn.qs('.cube');
-console.log('대상: ', cube);
+// 정보표시 박스 : .cat-info
+const infoBox = domFn.qs('.cat-info');
+console.log('대상: ', cube, infoBox);
+
 
 // 2. 이벤트 설정하기
 domFn.addEvt(window,'wheel',rotateMem);
@@ -46,28 +60,61 @@ function rotateMem() {
     // 0. 휠 이벤트 발생수 조절하기(광휠금지)
     if(stsWheel) return; // 막기
     stsWheel = 1; // 잠금!
-    setTimeout(()=>stsWheel=0,)
+    setTimeout(()=>stsWheel=0,TIME_WHEEL); // 해제
 
     // 1. 휠 방향 알아내기 : 휠 델타
     let delta = event.wheelDelta;
     
     // 2. 방향에 따른 회전 각도 만들기
     if(delta<0) {
-        // 휠 단위수 증가
-        numWheel++;
-    }
-    else {
         // 휠 단위수 감소
         numWheel--;
+        // 캐릭터 고유번호 증가(한계수에서 0으로 순환!)
+        catNum++;
+        if(catNum>LIMIT_CNT)catNum=0;
+    }
+    else {
+        // 휠 단위수 증가
+        numWheel++;
+        
+        // 캐릭터 고유번호 감소(한계수에서 끝번호로 순환!)
+        catNum--;
+        if(catNum<0)catNum=LIMIT_CNT;
     }
     // 호출 확인
-    console.log('휠~~~~~~~~~~~~~~~~~~~~', delta,numWheel);
+    // console.log('휠~~~~~~~~~~~~~~~~~~~~', delta,numWheel);
+    console.log("캐릭터 고유번호: ",catNum);
+    console.log("캐릭터 데이터: ",mvData[catNum]);
 
     // 3. 회전 대상 요소에 각도 적용하기
     // 적용각도 = 단위 각도 * 휠 단위수
     cube.style.transform = `rotateY(${numWheel*DEG}deg)`;
+
+    // 4. 캐릭터가 정지한 지 .5초 후 정보를 셋팅함!
+    // 타임아웃 실행 쓰나미 방지 지우기
+    clearTimeout(autoT);
+    // .5초 후 캐릭터 정보 셋팅함수 호출!
+    autoT = setTimeout(showInfo,500);
+
+    // 5. 기본 캐릭터 정보 박스 클래스 on 지우기
+    infoBox.classList.remove('on');
     
 } ////////// rotateMem 함수 //////////
+
+/****************************************************
+    함수명: showInfo
+    기능: 캐릭터 정보를 화면에 표시한다
+****************************************************/ 
+function showInfo() {
+    console.log('보여줄게!', mvData[catNum]);
+
+    // 정보표시 박스에 h2, p요소 정보와 함께 넣기
+    infoBox.innerHTML = `
+    <h2>${mvData[catNum].name}</h2>
+    <p>${mvData[catNum].desc}</p>`;
+    // 등장 클래스 on 주기
+    infoBox.classList.add('on');
+}; /////////////////// showInfo 함수 /////////////////////
 /************************************* 
     [구현내용]
     - "돌아!"버튼을 클릭하면 멈춰있던
--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -1,81 +1,82 @@
 // 도깨비 PJ 메인 JS - main.js
 
-// 모듈 불러오기 //////////////
-// DOM 함수 모듈
-import dFn from './dom.js';
+// 모듈 불러오기 ///////
+// DOM함수 모듈
+import dFn from "./dom.js";
 // 부드러운 스크롤 모듈
-import {startSS,setPos} from './smoothScroll23.js';
+import { startSS, setPos } from "./smoothScroll23.js";
 // 데이터 모듈
-import {gridData,gnbData, previewData} from './data_drama.js';
+import { gridData, gnbData, previewData, clipData } from "./data_drama.js";
 
-// 부드러운 스크롤 적용 ///////////////////
-    startSS();
+// 부드러운 스크롤 적용 //////////
+startSS();
 
-    // 0. 새로고치면 스크롤바 위치캐싱후 맨위로 이동
+// 0. 새로고치면 스크롤바 위치캐싱후 맨위로 이동
 setTimeout(() => {
-    // 윈도우 스크롤 맨위로!
-    window.scrollTo(0, 0);
-    // 부드러운 스크롤 위치값 반영!
-    setPos(0);
-    // 안하면 원래 위치로 스크롤시 튐!
-  }, 400);
-  // 0. 스크롤바 트랙을 잡고 위치이동시 위치값 반영
-  dFn.addEvt(window, "mouseup", () => setPos(window.scrollY));
-  //////// mouseup /////////////
-  
-  // 0. 키보드 방향키 이동시 위치값 반영
-  dFn.addEvt(window, "keyup", () => setPos(window.scrollY));
-  //////// mouseup /////////////
+  // 윈도우 스크롤 맨위로!
+  window.scrollTo(0, 0);
+  // 부드러운 스크롤 위치값 반영!
+  setPos(0);
+  // 안하면 원래 위치로 스크롤시 튐!
+}, 400);
+// 0. 스크롤바 트랙을 잡고 위치이동시 위치값 반영
+dFn.addEvt(window, "mouseup", () => setPos(window.scrollY));
+//////// mouseup /////////////
 
-    // 부드러운 스크롤 때문에 마우스휠 이벤트 막기가
-    // 작동되어 캐릭터 걸명 박스 작은 스크롤도 작동 안됨!
-    // 따라서 이벤트 버블링을 막아줘야 함!
-    // event.stopPropagation()
-    // 이벤트 객체의 이벤트 버블링 막아주는 메서드임!
+// 0. 키보드 방향키 이동시 위치값 반영
+dFn.addEvt(window, "keyup", () => setPos(window.scrollY));
+//////// mouseup /////////////
 
-    // 대상: .desc-box
-    let desc_box = document.querySelectorAll(".desc-box");
-    // console.log(desc_box);
+// 부드러운 스크롤 때문에 마우스휠 이벤트막기가
+// 작동되어 캐릭터설명박스 작은 스크롤도 작동안됨!
+// 따라서 이벤트 버블링을 막아줘야함!
+// event.stopPropagation()
+// 이벤트 객체의 이벤트 버블링 막아주는 메서드임!
 
-    // 모든 캐릭터 설명박스는 이벤트 버블링 막기
-    // -> 여기서 마우스휠 됨!
-    desc_box.forEach((ele) => {
-        /* ele - 요소 자신 */
-        ele.onwheel = (e) => e.stopPropagation();
-    });
+// 대상: .desc-box
+let desc_box = document.querySelectorAll(".desc-box");
+// console.log(desc_box);
 
-    /**********************************************
-    [ 현장포토 파트 데이터 구성하기 ]
-    - 배열 데이터를 이용하여 HTML 코드 구성
-**********************************************/
+// 모든 캐릭터 설명박스는 이벤트 버블링 막기!!!
+// -> 여기서 마우스휠 됨!!!
+desc_box.forEach((ele) => {
+  // ele - 요소자신
+  ele.onwheel = (e) => e.stopPropagation();
+});
 
-    // 1. 대상 선정 : .grid-box(.live-box / .poster-box)
-    const gridBox = dFn.qsa('.grid-box');
-    // console.log('대상: ', gridBox);
+/**************************************** 
+    [ 그리드박스 공통파트 데이터 구성하기 ]
+    - 배열데이터를 이용하여 HTML코드 구성
+  ****************************************/
+// 1. 대상선정 : .grid-box (.live-box/.poster-box)
+const gridBox = dFn.qsa(".grid-box");
+// console.log("대상:", gridBox);
 
-    // 2. 대상 코드 넣기 함수 호출 설정하기 ////////
-    gridBox.forEach((ele,idx)=>makeGrid(ele,idx));
+// 2. 대상 코드넣기 함수 호출설정하기 ///////
+gridBox.forEach((ele, idx) => makeGrid(ele, idx));
 
-    // 3. 그리드 스타일 데이터 생성하기 함수
-    function makeGrid(ele,idx) {
-        // ele - 대상요소 / idx - 순번(데이터 순번)
-        // 1. 현장 포토 데이터를 기반으로 HTML 코드 만들기
-        let hcode = "<ul>";
+// 3. 그리드 스타일 데이터 생성하기 함수
+function makeGrid(ele, idx) {
+  // ele - 대상요소 / idx - 순번(데이터순번)
+  // 1. 현장포토 데이터를 기반으로 HTML코드 만들기
+  let hcode = "<ul>";
 
-        // 반복코드 만들기 ////////
-        // 현장포토 데이터 - daa_drama.js에서 가져옴
-        gridData[idx].forEach((val) => {
-            // html변수에 계속 넣기
-    // 폴더경로는 idx가 0이면'live_photo' 
+  // 반복코드 만들기 /////
+  // 현장포토 데이터 - data_drama.js에서 가져옴
+  gridData[idx].forEach((val) => {
+    // html변수에 계속 넣기
+    // 폴더경로는 idx가 0이면'live_photo'
     // 1이면'poster_img'로 셋팅함!
     hcode += ` <li>
               <figure>
-              <img src="images/${idx?'poster_img':'live_photo'}/${val.imgName}.jpg" alt="${val.title}">
-              <figcaption>${val.title}</figcaption>
+                  <img src="images/${idx ? "poster_img" : "live_photo"}/${
+      val.imgName
+    }.jpg" alt="${val.title}">
+                  <figcaption>${val.title}</figcaption>
               </figure>
-              </li>
-              `;
-            }); //////// forEach /////////////
+          </li>
+        `;
+  }); //////// forEach /////////////
 
   hcode += "</ul>";
 
@@ -131,114 +132,202 @@ gnbList.forEach((ele) => {
   } ////////// if /////////
 }); ///////// forEach ////////////
 
-/*************************************************
-    [ 상위메뉴 li 오버 시 하위메뉴 보이기 ]
-    이벤트 대상: .gnb>ul>li
-    변경 대상: .smenu
-*************************************************/ 
-// 1. 대상 선정
+/************************************* 
+  [ 상위메뉴 li오버시 하위메뉴 보이기 ]
+  이벤트 대상: .gnb>ul>li
+  변경 대상: .smenu
+*************************************/
+// 1. 대상선정
 const gnb = dFn.qsa(".gnb>ul>li");
 
 // 2. 이벤트 설정하기
 // 이벤트 종류: mouseover / mouseout
 gnb.forEach((ele) => {
-    // 서브메뉴가 있을 때만 이벤트 설정하기!
-    // if문에서 undefined/null은 false처리됨!
-    if(dFn.qsEl(ele,".smenu")) {
-        dFn.addEvt(ele, "mouseover", overFn);
-        dFn.addEvt(ele, "mouseout", outFn);
-    }
+  // 서브메뉴가 있을때만 이벤트 설정하기!
+  // if문에서 undefined/null 은 false처리됨!
+  if (dFn.qsEl(ele, ".smenu")) {
+    dFn.addEvt(ele, "mouseover", overFn);
+    dFn.addEvt(ele, "mouseout", outFn);
+  }
 });
 
-// 3. 함수만들기
+// 3.함수만들기
 function overFn() {
-    // console.log('오버', this);
-    // 1. 하위 .smbx 높이값 알아오기
-    let hv = dFn.qsEl(this, ".smbx").clientHeight;
-    // console.log("높이:", hv);
-    // 2.하위 서브메뉴박스 만큼 .smenu 높이값 주기
-    dFn.qsEl(this, ".smenu").style.height = hv + "px";
-  } //////////// overFn 함수 ////////////
-  
-  function outFn() {
-    // // console.log('아웃',this);
-    // 서브메뉴 박스 높이값 0만들기!
-    dFn.qsEl(this, ".smenu").style.height = "0px";
-  } //////////// outFn 함수 ////////////
+  // // console.log('오버',this);
+  // 1.하위 .smbx 높이값 알아오기
+  let hv = dFn.qsEl(this, ".smbx").clientHeight;
+  // console.log("높이:", hv);
+  // 2.하위 서브메뉴박스 만큼 .smenu 높이값 주기
+  dFn.qsEl(this, ".smenu").style.height = hv + "px";
+} //////////// overFn 함수 ////////////
 
+function outFn() {
+  // // console.log('아웃',this);
+  // 서브메뉴 박스 높이값 0만들기!
+  dFn.qsEl(this, ".smenu").style.height = "0px";
+} //////////// outFn 함수 ////////////
 
-  ///////////////////////////////////////////////////
-  // 인트로 동영상 클릭 시 플레이하기
-  // 대상 : .intro-mv-img
-  // 이벤트 : click
-  // -> 가상요소 플레이 버튼 클릭 시
-  // 이벤트 버블링으로 본 박스가 반응함!
+/////////////////////////////////////
+/// 인트로 동영상 클릭시 플레이하기 ////
+// 대상: .intro-mv-img
+// 이벤트: click
+// -> 가상요소 플레이버튼 클릭시
+// 이벤트 버블링으로 본 박스가 반응함!
 
-  // 1. 대상 선정하기
-  const mvBox = dFn.qs('./intro-mv-img');
+// 1. 대상 선정하기
+const mvBox = dFn.qs(".intro-mv-img");
 
-  // 2. 이벤트 설정하기
-dFn.addEvt(mvBox,'click',showMv);
+// 2. 이벤트 설정하기
+dFn.addEvt(mvBox, "click", showMv);
 
-// 이벤트 연결 상태 변수(한번만 실행키 위한 변수)
+// 이벤트연결 상태변수(한번만 실행키위한 변수)
 let stsShowMv = 0;
 
 // 3. 함수만들기
 function showMv() {
-  if(stsShowMv) return; // 돌아가~
-  stsShowMv = 1; // 한번만 실행
-  // console.log('보여줘~!!');
+  if (stsShowMv) return; // 돌아가!
+  stsShowMv = 1; // 한번만실행
+
+  // console.log('보여줘~!!!!!');
   // 동영상 넣기
   // 대상: 나자신(.intro-mv-img)
   this.innerHTML = `
-  <video src='./images/intro_mv.mp4' autoplay controls></video>
+    <video src='./images/intro_mv.mp4' autoplay controls></video>
   `;
 
-  // 가상요소 플레이버튼 없애기 위해 .off지우기
-  this.classList.remove('off');
+  // 가상요소 플레이버튼 없애기위해 .off지우기
+  this.classList.remove("off");
+} ///////// showMv 함수 ///////////
 
-  // 이벤트 등록 지우기하여 다음 클릭 시 이 함수 연결끊기!
-  mvBox.removeEventListener(showMv);
-} ////////////////// showMv 함수 /////////////////////////
-
-
-///////////////////////////////////////////////////////////
-// 오름차순 데이터를 내림차순으로 변경하여 화면에 뿌리기
+/////////////////////////////////////////////
+// 오름차순 데이트를 내림차순으로 변경하여 화면에 뿌리기!
 
 // 1. 데이터 정렬 변경하기
-let preNewData = 
-previewData.sort((x,y)=>{
+let preNewData = previewData.sort((x, y) => {
   // x,y는 배열값 앞뒤를 계속 가지고 들어옴
-  // 배열값 중 idx 속성값을 가져와서 숫자형 변환 후 사용
+  // 배열값 중 idx속성값을 가져와서 숫자형변환후 사용
   let a = Number(x.idx);
   let b = Number(y.idx);
 
-  // 배열 순서 변경 메서드인 sort() 내부에 return값을 
+  // 배열 순서변경 메서드인 sort() 내부에 return값을
   // 사용하여 순서를 변경한 새로운 배열을 만들어준다!
-  return a == b ? 0 : (a > b ? -1 : 1)
+  return a == b ? 0 : a > b ? -1 : 1;
   // 비?집:(눈?집:놀이동산)
 });
 // console.log(preNewData);
 
-// 2. 대상 선정: .preview-box>div
-const preBox = dFn.qsa('.preview-box>div');
+// 2. 대상선정: .preview-box>div
+const preBox = dFn.qsa(".preview-box>div");
 // console.log(preBox);
 
-// 3. 대상을 순회하여 태그넣기
+// 3. 대상을 순회하여 태그 넣기
 // 데이터 : 역순정렬을 한 미리보기 데이터넣기
-
-preBox.forEach((ele,idx)=>{
+preBox.forEach((ele, idx) => {
   ele.innerHTML = `
-  <h3>${preNewData[idx].title}</h3>
-  <p>${preNewData[idx].story}</p>
+    <div>
+      <h3>${preNewData[idx].title}</h3>
+      <p>${preNewData[idx].story}</p>
+    </div>
   `;
-}); //////////////// forEach /////////////////////
+}); //////// forEach ///////////////
 
-//////////////////////////////////////////////////////////
-////////////////////// 최신 동영상 영역 데이터 뿌리기 ///////////////////////
+///////////////////////////////////////////////
+///////// 최신 동영상 영역 데이터 뿌리기 ////////
 // 대상: .clip-box
-const clipBox = dFn.qs('.clip-box');
+const clipBox = dFn.qs(".clip-box");
 console.log(clipBox);
 
-// 데이터 매칭하여 태그 만들기
-const clip
+// 생성할 데이터
+let clipCode = "";
+
+// 데이터 매칭하여 태그만들기
+// 배열데이터 이므로 forEach사용!
+clipData.forEach((val) => {
+  clipCode += `
+    <li>
+      <div class="clip-mv-box">
+        <img src="./images/clip_img/${val.idx}.jpg" alt="${val.subtit}">
+      </div>
+      <h4>${val.subtit}</h4>
+      <h3>${val.title}</h3>
+    </li>
+  `;
+}); ////////// forEach /////////
+
+console.log(clipCode);
+
+// 코드 넣기
+clipBox.innerHTML = `<ul>${clipCode}</ul>`;
+
+//////// 최신동영상 파트 이동기능 구현 //////////////
+// 1. 요구사항 : 버튼 한번에 한 영상씩 이동
+//              양쪽끝에가면 이동중단 해당방향 버튼 사라짐!
+
+// 2. 대상선정
+// 2-1. 이벤트 대상 : .btn-box button
+const btnClip = dFn.qsa(".btn-box button");
+
+// 2-2. 변경대상 : .clip-box ul
+const clipList = dFn.qs(".clip-box ul");
+
+// 3. 변수셋팅 ////////////////////////
+// 3-1.리스트 개수
+const CNT_LIST = dFn.qsaEl(clipList, "li").length;
+// 3-2.화면당 리스트노출 개수
+const LIMIT_LIST = 4;
+// 3-3.이동 한계수
+const LIMIT_MOVE = CNT_LIST - LIMIT_LIST;
+// 3-4.이동 단위수 : 간격이동까지 고려한 한번에 이동할 단위 -25.5%
+const BLOCK_NUM = 25.5;
+// 3-5.이동회수 : 단위만큼 이동할 횟수
+let mvNum = 0;
+
+// console.log(btnClip,clipList,'이동 한계수:',LIMIT_MOVE);
+
+// 4. 이벤트 셋팅하기 ///////////////
+btnClip.forEach((ele) => {
+  dFn.addEvt(ele, "click", moveClip);
+}); //////////// forEach ///////////
+
+// 5. 함수 만들기 ////////////////
+function moveClip() {
+  // 1. 오른쪽 버튼 여부
+  let isR = this.classList.contains("fa-chevron-right");
+  console.log("나야나!", isR);
+  // 2. 버튼별 이동분기
+  if (isR) {
+    // 오른쪽버튼
+    // 이동한계수를 체크하여 이동수를 증가시킴
+    mvNum++;
+    // 마지막한계수를 넘어가면 마지막 수에 고정!
+    if (mvNum > LIMIT_MOVE){ 
+      // 마지막수 고정
+      mvNum = LIMIT_MOVE;
+      // 마지막버튼 숨기기
+      btnClip[1].style.display = 'none';
+    }
+    else{
+      // 첫번째버튼 보이기
+      btnClip[0].style.display = 'block';
+    }
+  } //////// if ////////
+  else {
+    // 왼쪽버튼
+    // 이동한계수를 체크하여 이동수를 감소시킴
+    mvNum--;
+    // 첫번째한계수를 넘어가면 0에 고정!
+    if (mvNum < 0){ 
+      // 0에 고정
+      mvNum = 0;
+      // 첫번째버튼 숨기기
+      btnClip[0].style.display = 'none';
+    }
+    else{
+      // 마지막버튼 보이기
+      btnClip[1].style.display = 'block';
+    }
+  } //////// if ////////
+
+  // 3. 이동반영하기 : - (단위수*이동수) %
+  clipList.style.left = -(BLOCK_NUM * mvNum) + "%";
+} /////////// moveClip 함수 //////////
