9bbe253200572a1f48bd694575ecc0f2237cfebe
kwonyounghye <kyh90721@gmail.com>
2023-10-13 18:16:59 +0900

    1013

--- 003.JS학습/06.JS달력/js/calendar.js
+++ 003.JS학습/06.JS달력/js/calendar.js
@@ -4,8 +4,11 @@
 const dFn = {
     qs: (x) => document.querySelector(x),
     qsa: (x) => document.querySelectorAll(x),
+    qsEl: (el, x) => el.querySelector(x),
+    qsaEl: (el, x) => el.querySelectorAll(x),
+    addEvt: x => document.addEventListener(x),
     cg: (x) => console.log(x),
-    addZero: x => x<
+    addZero: x => x < 10 ? '0' + x : x,
     fm: (x) => `
         ${x.getFullYear()}-${x.getMonth() + 1 < 10 ? "0" + (x.getMonth() + 1) : x.getMonth() + 1}-${
         x.getDate() < 10 ? "0" + x.getDate() : x.getDate()
@@ -32,8 +35,10 @@ function makeDallyeok() {
     const monthTit = dFn.qs(".monthTit");
     // (5) 날짜요소 : .dates
     const dates = dFn.qs(".dates");
-    // (6)
+    // (6) 날짜  넣을 배열변수
     const dateSet = [];
+    // (7) htmlㅋ 코드 저장 변수
+    let hcode = '';
 
     // dFn.cg(yearTit);
     // dFn.cg(monthTit);
@@ -42,6 +47,18 @@ function makeDallyeok() {
     // 2. 함수 만들기 ///////////////////
     // (1) 달력 초기화구성 함수 ///////
     const initDallyeok = () => {
+        // 변수 초기화
+        // 날짜 배열 초기화 : splice(시작순번, 개수)
+        // -> 배열변수.splice(0) 첫배열부터 모두 지움!
+        dateSet.splice(0);
+        // html코드 변수 초기화
+        hcode = '';
+
+        // 현재년
+        let cYr = currDate.getFullYear();
+        // 현재달
+        let cMt = currDate.getMonth();
+
         // [선택달  끝날짜, 첫날짜 알아오기]
         // new Date(년도,월,옵션)
         // (1) 년도
@@ -50,9 +67,29 @@ function makeDallyeok() {
 
         // 1. 전달 마지막 날짜(옵션:0)
         // -> 달력처음 전달끝쪽날짜표시
+        const prevLast = 
+        new Date(cYr,cMt,0);
+
+        dFn.cg('전달끝날짜:'+dFn.fm(prevLast));
+
+        // 2. 현재달 첫째날짜(옵션:1->전달로 셋팅)
+        // -> 달력 전달셋팅을 위한 요일 구하기 위해!
+        const thisFirst = 
+        new Date(cYr,cMt,1);
+            
+        dFn.cg('현재달 첫째날짜:'+dFn.fm(thisFirst));
+
+        // 3. 현재달 마지막날짜(옵션:0)
+        const thisLast = new Date(cYr,cMt+1,0);
+        
+        dFn.cg('현재달 마지막날짜:'+dFn.fm(thisLast));
+
+        // 4. 년도표시하기
+        yearTit.innerHTML = cYr;
 
         // 5. 월 표시하기
-        monthTit.innerHTML = cMT + 2;
+        monthTit.innerHTML = cMt + 1;
+
         // 6. 날짜 데이터 태그 구성하기
         // 6-1. 전달 날짜 앞쪽 채우기
         // 조건: 현재달 첫날짜 요일이 0이 아니면 내용 있음!
@@ -69,22 +106,112 @@ function makeDallyeok() {
                         ${prevLast.getDate() - i}
             </span>
             `);
+            // 전달 마지막 날짜 - for문 i값(0, 1, 2, ...)
+
             } /////////// for ////////////
         } ////////////// if ////////////////////
+         // 6-2. 현재월 삽입하기 ///////////
+        // 반복문 구성: 현재월 1일부터 마지막날짜까지 반복 배열추가
+        for(let i = 1; i <= thisLast.getDate(); i++){
+            dateSet.push(i);
+        } ///////////// for ////////////////////
+
 
         // 6-3. 다음달 나머지 칸 삽입하기 ///////
         // 다음달은 클래스 'am'으로 구분
         // 반복구성: 1부터 2주 분량정도 넣는다!
         for(let i = 1; i <= 14; i++) {
-            dateSet.push(i);
+            dateSet.push(`
+            <span style="color:#ccc" class="am">
+                ${i}
+            </span>
+        `);
         } ////////////// for ///////////
 
         dFn.cg('날짜 배열: '+dateSet);
 
         // 7. 날짜 배열로 날짜 태그 구성하여 출력하기
         // 7일 * 6주 = 42개
-        dates.innerHTML = dateSet.map((v,i)=>i<42?`<div class="date">${v}</div>`:'').join('');
+        for(let i=1; i<=42; i++) {
+            // 오늘 날짜와 같은 경우 클래스 "today" 넣기
+            if(
+                // [년, 월, 일이 모두 일치하는 오늘만 표시]
+                // (1) 오늘 날짜 == 배열값 날짜 AND
+                today.getDate() == dateSet[i] &&
+                // (2) 현재달 == 선택달 AND
+                today.getMonth() == currDate.getMonth() &&
+                // (3) 현재년도 == 선택년도
+                today.getFullYear() == currDate.getFullYear()
+            ) {
+                hcode += `<div class=date today>${dateSet[i]}</div>`
+            } /////////// if ///////
+            else {
+
+            } ////////// else //////////
+        }
+        // dates.innerHTML = dateSet.map((v,i)=>i<42?`<div class="date">${v}</div>`:'').join('');
+
+        // (1) 대상선정: .date -> 위에서 새로 담겼으므로 새로 읽음!
+        let newDate = dFn.qsa('.date');
+        // console.log(newDate);
+
+        // (2) 각 날짜 .date요소에 링크 설정하기
+        newDate.forEach(ele=>{
+            dFn.addEvt(ele, 'click', ()=>{
+                console.log('나야나');
+                // 1. 연도 읽기
+                let nowY = yearTit.innerText;
+                // 2. 월 읽기
+                let nowM = monthTit.innerText;
+                // 3. 날짜 읽기
+                let nowD = ele.innerText;
+
+                // 4. 전달/다음달 구분하기
+                let isSpan = dFn.qsEl(ele, 'span');
+                console.log('span있니?', isSpan);
+                // span이 있으면 true처리됨!
+                if(isSpan) {
+                    // span의 클래스가 'bm'/'am' 인지 구분하기
+                    let isAM = isSpan.classList.contains('am');
+                    if(isAM) { // 다음달이므로 1 더함
+                        nowM++;
+                        if(nowM==13) {    // 13월은 1월로 처리
+                            nowM = 1;
+                            // 1월은 다음해로 처리
+                            nowY++;
+                        }
+                    }
+                    else { // 'bm'일 경우 즉, 전달!
+                        nowM--;
+                        
+
+                    }
+                }
+
+                console.log(`${}`)
+            })
+        })
     }; /////// initDallyeok 함수 ////////
+
+    // (2) 이전/다음 달력 출력하기 함수 /////////////////////
+    const prevCalendar = () => {
+        console.log('이전달력 고고!');
+        // 이전달로 변경하여 initDallyeok() 함수 호출!
+        // getMonth() 월 가져오기 / setMonth() 월 셋팅하기!
+        currDate.setMonth(currDate.getMonth()-1);
+        initDallyeok();
+    }; /////////// prevCalender 함수 ////////////////////
+
+    // (3) 다음달력 출력하기 함수 ////////////////////
+    const nextCalendar = () => {
+        console.log('다음달력 고고!');
+    }; /////////////// nextCalendar 함수 //////////////////
+
+    // 3. 이벤트 설정하기 //////////////
+    // 이전버튼에 함수 연결하기 :  달을 빼기 위해 -1 전달
+    dFn.addEvt(dFn.qs('.btnL'), 'click', ()=>chgCalendar(-1));
+    // 다음버튼에 함수연결하기 : 달을 더하기 위해 1 전달
+    dFn.addEvt(dFn.qs('.btnR'), 'click', ()=>chgCalendar(1));
 
     // 초기 셋팅 함수 호출
     initDallyeok();
