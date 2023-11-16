9e6ad41272583c53e783ce5ef1bb73ee523543dd
kwonyounghye <kyh90721@gmail.com>
2023-11-16 19:03:02 +0900

    1116

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현코드/dc-app/src/components/pages/Comics.jsx
@@ -0,0 +1,13 @@
+// 코믹스페이지 메인컨텐츠
+
+import { Banner } from "../modules/Banner";
+
+export function Comics(){
+    return(
+        <>
+            <VidIntro cat="MOVIES">
+            <Banner category="COMICS" />
+        </>
+    )
+
+} ////////////  Comics 컴포넌트 ///////////
